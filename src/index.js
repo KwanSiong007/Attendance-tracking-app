779ba249fdf03a4d7ee106a01ebac97918e339e5
chengwaikoo <chengwai.koo@gmail.com>
2023-09-19 21:26:15 +0800

    feat: add simple login and register pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import LogIn from "./pages/LogIn";
+import Register from "./pages/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <LogIn /> */}
+    <Register />
   </React.StrictMode>
 );

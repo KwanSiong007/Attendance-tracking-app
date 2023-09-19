71ce9618139143b10f4057a2e276c5929292f72c
chengwaikoo <chengwai.koo@gmail.com>
2023-09-19 20:07:50 +0800

    chore: remove unnecessary logos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
 
 class App extends React.Component {
@@ -7,7 +6,6 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>

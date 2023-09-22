bc62fbb4b7138929f67371c91c7cbb30fef01f6a
KwanSiong007 <ksong329@gmail.com>
2023-09-22 20:58:42 +0800

    feat: set up authentication in Login page and display logged in user data in worker screen

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import LogIn from "./pages/LogIn";
-import Register from "./pages/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    {/* <LogIn /> */}
-    {/* <Register /> */}
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
--- src/pages/WorkerScreen.js
+++ src/pages/WorkerScreen.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { onChildAdded, ref } from "firebase/database";
+import { database } from "../firebase";
+
+const DB_LOGGED_IN_USER_KEY = "logged_in_user";
 
 function WorkerScreen() {
+  const [loggedInUser, setLoggedInUser] = useState([]);
+
+  useEffect(() => {
+    const loggedInUserRef = ref(database, DB_LOGGED_IN_USER_KEY);
+    onChildAdded(loggedInUserRef, (data) => {
+      setLoggedInUser((prevData) => [
+        ...prevData,
+        { key: data.key, val: data.val() },
+      ]);
+    });
+  }, []);
+
+  console.log("loggedInUser:", loggedInUser);
+
   return <div>WorkerScreen</div>;
 }
 

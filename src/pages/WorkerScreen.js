19ff6e52b93f327d3acf4872bfac85d54cc7be49
chengwaikoo <chengwai.koo@gmail.com>
2023-09-26 20:48:42 +0800

    feat: add barebones UI

--- src/pages/WorkerScreen.js
+++ src/pages/WorkerScreen.js
@@ -1,4 +1,12 @@
 import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { onChildAdded, ref } from "firebase/database";
 import { database } from "../firebase";
 
@@ -19,7 +27,61 @@ function WorkerScreen() {
 
   console.log("loggedInUser:", loggedInUser);
 
-  return <div>WorkerScreen</div>;
+  // Assuming the GPS status and location data will be fetched and stored in state
+  const gpsStatus = "off"; // This could be 'off', 'on-site', or 'on-other'
+  const siteName = "Site A"; // Name of the site if GPS is 'on-site'
+
+  const checkIns = [
+    { location: "Site A", checkInTime: "08:00 AM", checkOutTime: "05:00 PM" },
+    // ...other check-ins
+  ];
+
+  let statusText;
+  switch (gpsStatus) {
+    case "on-site":
+      statusText = `GPS is on. You are at ${siteName}.`;
+      break;
+    case "on-other":
+      statusText = "GPS is on. You are not at any site.";
+      break;
+    default:
+      statusText = "GPS is off.";
+  }
+
+  return (
+    <>
+      <div>{statusText}</div>
+      <Button
+        variant="contained"
+        sx={{
+          borderRadius: "50%",
+          width: "100px",
+          height: "100px",
+          textTransform: "none",
+        }}
+      >
+        Check In
+      </Button>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Location</TableCell>
+            <TableCell>Check-in Time</TableCell>
+            <TableCell>Check-out Time</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {checkIns.map((checkIn, index) => (
+            <TableRow key={index}>
+              <TableCell>{checkIn.location}</TableCell>
+              <TableCell>{checkIn.checkInTime}</TableCell>
+              <TableCell>{checkIn.checkOutTime}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  );
 }
 
 export default WorkerScreen;

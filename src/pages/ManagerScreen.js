7fdeb32366a4ace24a2beefe22303a56b6a384a6
KwanSiong007 <ksong329@gmail.com>
2023-10-08 21:14:07 +0800

    feat: added search bar in ManagerScreen

--- src/pages/ManagerScreen.js
+++ src/pages/ManagerScreen.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import { ref, onValue } from "firebase/database";
 import { database } from "../firebase";
 import ManagerAttendance from "../components/ManagerAttendance";
+import { TextField, InputAdornment, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const DB_ATTENDANCE_RECORDS_KEY = "action";
 
 function ManagerScreen() {
   const [nowLoaded, setNowLoaded] = useState(null);
-
   const [attendance, setAttendance] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredAttendance, setFilteredAttendance] = useState([]);
 
   useEffect(() => {
     const nowLoaded = new Date();
@@ -30,8 +33,9 @@ function ManagerScreen() {
           (a, b) =>
             Date.parse(b.checkInDateTime) - Date.parse(a.checkInDateTime)
         );
-
+        console.log("sortedAttendance:", sortedAttendance);
         setAttendance(sortedAttendance);
+        setFilteredAttendance(sortedAttendance);
       },
       {
         onlyOnce: false,
@@ -41,7 +45,41 @@ function ManagerScreen() {
     return () => unsubscribe();
   }, []);
 
-  return <ManagerAttendance attendance={attendance} nowLoaded={nowLoaded} />;
+  const handleSearchChange = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query);
+    const filteredRecords = attendance.filter((record) =>
+      record.username.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredAttendance(filteredRecords);
+  };
+
+  return (
+    <>
+      <div id="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          label="Search"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </div>
+      <ManagerAttendance
+        attendance={filteredAttendance}
+        nowLoaded={nowLoaded}
+      />
+    </>
+  );
 }
 
 export default ManagerScreen;

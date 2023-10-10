cc34375589d3cf93bcc563c1b1122e1be3e07c58
chengwaikoo <chengwai.koo@gmail.com>
2023-10-10 20:56:49 +0800

    feat: only check in/out after holding for 1s

--- src/components/CheckInButton.js
+++ src/components/CheckInButton.js
@@ -1,16 +1,33 @@
+import React, { useRef } from "react";
 import { Button } from "@mui/material";
 
+const HOLD_DURATION = 1000; // in milliseconds
+
 function CheckInButton({ handleCheckIn }) {
+  const timeoutRef = useRef(null);
+
+  const handleMouseDown = () => {
+    timeoutRef.current = setTimeout(() => {
+      handleCheckIn();
+    }, HOLD_DURATION);
+  };
+
+  const handleMouseUp = () => {
+    clearTimeout(timeoutRef.current);
+  };
+
   return (
     <Button
-      onClick={handleCheckIn}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
       variant="contained"
       sx={{
         borderRadius: "50%",
         width: "160px",
         height: "160px",
         fontSize: "h5.fontSize",
-        lineHeight: "1.2",
+        lineHeight: "1.5",
         textTransform: "none",
         backgroundColor: "darkgreen",
         "&:hover": {
@@ -18,7 +35,7 @@ function CheckInButton({ handleCheckIn }) {
         },
       }}
     >
-      Check In
+      Hold to Check In
     </Button>
   );
 }
--- src/components/CheckOutButton.js
+++ src/components/CheckOutButton.js
@@ -1,16 +1,33 @@
+import React, { useRef } from "react";
 import { Button } from "@mui/material";
 
+const HOLD_DURATION = 1000; // in milliseconds
+
 function CheckOutButton({ handleCheckOut }) {
+  const timeoutRef = useRef(null);
+
+  const handleMouseDown = () => {
+    timeoutRef.current = setTimeout(() => {
+      handleCheckOut();
+    }, HOLD_DURATION);
+  };
+
+  const handleMouseUp = () => {
+    clearTimeout(timeoutRef.current);
+  };
+
   return (
     <Button
-      onClick={handleCheckOut}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
       variant="contained"
       sx={{
         borderRadius: "50%",
         width: "160px",
         height: "160px",
         fontSize: "h5.fontSize",
-        lineHeight: "1.2",
+        lineHeight: "1.5",
         textTransform: "none",
         backgroundColor: "darkred",
         "&:hover": {
@@ -18,7 +35,7 @@ function CheckOutButton({ handleCheckOut }) {
         },
       }}
     >
-      Check Out
+      Hold to Check Out
     </Button>
   );
 }
